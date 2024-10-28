ad65af9696e6d5823cd83975b1d1b3b22d350160
Killian <killian.delamarre-ruer@students.dominican.edu>
2024-10-27 17:10:04 -0700

    Added acount number generator

--- bankaccount.py
+++ bankaccount.py
@@ -1,5 +1,10 @@
-class BankAccount:
+import random
+
+class BankAccount: #This function defines bank account and gives it 3 attributes, fullname, account number and balance.
     def __init__(self, full_name):
         self.full_name = full_name
         self.account_number = self.generate_account_number()
         self.balance = 0.0
+
+    def generate_account_number(self): #This is used to generate a random account number by importanting from random.
+        return random.randint(100000, 999999)
