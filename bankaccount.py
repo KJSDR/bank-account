2dc5ea03756cad4296b93c88679e0a4cfd7d7a87
Killian <killian.delamarre-ruer@students.dominican.edu>
2024-10-27 18:52:26 -0700

    added some functions and comments

--- bankaccount.py
+++ bankaccount.py
@@ -8,3 +8,13 @@ class BankAccount: #This function defines bank account and gives it 3 attributes
 
     def generate_account_number(self): #This is used to generate a random account number by importanting from random.
         return random.randint(100000, 999999)
+
+    def deposit(self, amount): #Here we define the deposite function and how it is updated
+        self.balance += ammount
+        print(f"Deposit Amount: ${amount:.2f} Balance: ${self.balance.2f}") #The .2f portion of the code specifies to only show the last 2 digits and rounds up. example 43.71927 turns into 43.72
+
+    def withdraw(self, amount): #Function for withdrawing and if you take too much money out get charged a fee for overdrafting.
+        if amount > self.balance: #Says that if the amount you are trying to withdraw is more than you have it will print "Insufficient funds and charge you a $10 fee"
+            print("Insufficient funds.")
+            self.balance -= 10
+            print(f"Overdraft fee is charged. Your new balance is: ${self.balance:.2f}")
